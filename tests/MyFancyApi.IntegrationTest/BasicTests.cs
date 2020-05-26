0c8539d56e4ad896006170163d13923afb68e219
Radu <randrei1@techteam.com>
2020-05-27 01:42:21 +0300

    Step 2a - Add crashing Unit Test

---
+++ tests/MyFancyApi.IntegrationTest/BasicTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyFancyApi.IntegrationTest
+{
+    public class BasicTests
+    : IClassFixture<WebApplicationFactory<MyFancyApi.Service.Startup>>
+    {
+        private readonly WebApplicationFactory<MyFancyApi.Service.Startup> _factory;
+
+        public BasicTests(WebApplicationFactory<MyFancyApi.Service.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]//we will route to some response
+        [InlineData("/api/values")]//will work because controller is implemented
+        [InlineData("/graphql")]//will work after graphql is added
+        [InlineData("/ui/playground")]//will work after graphql playground is added
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+        }
+    }
+}
