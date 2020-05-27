aeb32513f1a95c736e3b31ed6074c18248b087fa
Radu <randrei1@techteam.com>
2020-05-27 10:23:35 +0300

    Step 2f  - Add First Schema

--- src/MyFancyApi.Service/GraphQL/MyFancySchema.cs
+++ src/MyFancyApi.Service/GraphQL/MyFancySchema.cs
@@ -2,11 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using GraphQL;
 
 namespace MyFancyApi.Service.GraphQL
 {
     public class MyFancySchema : Schema
     {
+        public MyFancySchema(IDependencyResolver resolver) : base(resolver)
+        {
+            Query = resolver.Resolve<RootQuery>();
+        }
     }
 }
---
+++ src/MyFancyApi.Service/GraphQL/RootQuery.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFancyApi.Service.GraphQL
+{
+    public class RootQuery : ObjectGraphType
+    {
+        public RootQuery()
+        {
+            Field<StringGraphType>(
+                Name = "ping",
+                resolve: context => "I'm alive!"
+            );
+        }
+    }
+}
