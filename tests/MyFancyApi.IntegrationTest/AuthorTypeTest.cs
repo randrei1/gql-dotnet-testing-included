bb8645d185d34934acec3ed6f86bca79b0aae2a6
Radu <randrei1@techteam.com>
2020-05-27 15:28:21 +0300

    Add Failing GetAuthor Integration test

--- tests/MyFancyApi.IntegrationTest/AuthorTypeTest.cs
+++ tests/MyFancyApi.IntegrationTest/AuthorTypeTest.cs
@@ -6,12 +6,14 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MyFancyApi.IntegrationTest.Response;
 using MyFancyApi.Service;
 using MyFancyApi.Service.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +26,6 @@ namespace MyFancyApi.IntegrationTest
     {
         private readonly WebApplicationFactory<MyFancyApi.Service.Startup> _factory;
         private readonly ITestOutputHelper _testOutputHelper;
-        private LibContext _context;
 
         public AuthorTypeTest(WebApplicationFactory<MyFancyApi.Service.Startup> factory, ITestOutputHelper testOutputHelper)
         {
@@ -87,9 +88,35 @@ query {
 }"
             };
 
-            var graphQLResponse = await graphQLClient.SendQueryAsync<JObject>(authorQuery);
+            var graphQLResponse = await graphQLClient.SendQueryAsync<AuthorsResponse>(authorQuery);
             _testOutputHelper.WriteLine(JsonConvert.SerializeObject(graphQLResponse));
-            Assert.True(graphQLResponse.Errors == null);
+            Assert.True(graphQLResponse.Data.Authors.Any());
+        }
+
+        [Fact]
+        public async Task CanGetAuthorId()
+        {
+            var client = _factory.CreateClient();
+            client.BaseAddress = new Uri("http://localhost/");
+            var graphQLClient = CreateGraphQLClient();
+
+            var authorQueryWithVariables = new GraphQLRequest
+            {
+                Query = @"
+                    query($id: Int) {
+                        author(id: $id){
+                            name
+                        }
+                    }",
+                Variables = new
+                {
+                    id = 1
+                }
+            };
+
+            var graphQLResponse = await graphQLClient.SendQueryAsync<AuthorResponse>(authorQueryWithVariables);
+            _testOutputHelper.WriteLine(JsonConvert.SerializeObject(graphQLResponse));
+            Assert.False(string.IsNullOrEmpty(graphQLResponse?.Data?.author?.Name));
         }
 
         public Task DisposeAsync()
---
+++ tests/MyFancyApi.IntegrationTest/Response/AuthorResponse.cs
@@ -0,0 +1,12 @@
+﻿using MyFancyApi.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFancyApi.IntegrationTest.Response
+{
+    class AuthorResponse
+    {
+        public Author author { get; set; }
+    }
+}
